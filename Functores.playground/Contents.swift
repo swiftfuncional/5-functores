ab3102c41005cf06220c6936b75634fd533ce384
Swift Funcional <info@swiftfuncional.com>
2017-04-17 19:16:48 +0200

    Use TextField with pattern matching to send the username

--- Functores.playground/Contents.swift
+++ Functores.playground/Contents.swift
@@ -6,3 +6,16 @@ enum Optional<T> {
 class TextField {
 	let text: Optional<String> = .None
 }
+
+func send(_ username: String) {
+	//Logic to send username
+}
+
+func sendUserNameOrAlert(textField: TextField) {
+	switch textField.text {
+	case .None:
+		print("ERROR: TextField empty")
+	case let .Some(username):
+		send(username)
+	}
+}
