4b3f3cc88932ef5811fa02cbb0ffbd550bf48d51
Swift Funcional <info@swiftfuncional.com>
2017-04-18 00:31:56 +0200

    Create json with user information

--- Functores.playground/Contents.swift
+++ Functores.playground/Contents.swift
@@ -2,3 +2,10 @@ enum Optional<T> {
 	case None
 	case Some(_: T)
 }
+
+let configuration = "{" +
+	"\"name\":\"Alex\"," +
+	"\"accountType\":\"Premium\"," +
+	"\"email\":\"alex@swiftfuncional.com\"," +
+	"\"url\":\"www.swiftfuncional.com\","
+"}"
