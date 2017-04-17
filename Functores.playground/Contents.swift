5696a7660be97d687c48f68d09c5ce5ed90bc2b5
Swift Funcional <info@swiftfuncional.com>
2017-04-17 19:18:41 +0200

    Clear actual code to write notification error example

--- Functores.playground/Contents.swift
+++ Functores.playground/Contents.swift
@@ -2,20 +2,3 @@ enum Optional<T> {
 	case None
 	case Some(_: T)
 }
-
-class TextField {
-	let text: Optional<String> = .None
-}
-
-func send(_ username: String) {
-	//Logic to send username
-}
-
-func sendUserNameOrAlert(textField: TextField) {
-	switch textField.text {
-	case .None:
-		print("ERROR: TextField empty")
-	case let .Some(username):
-		send(username)
-	}
-}
