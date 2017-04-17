de992fff4942dbf9b682e721a52f1c72816cc20b
Swift Funcional <info@swiftfuncional.com>
2017-04-18 00:35:38 +0200

    Create Account struct and parse the user information

--- Functores.playground/Contents.swift
+++ Functores.playground/Contents.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 enum Optional<T> {
 	case None
 	case Some(_: T)
@@ -7,5 +9,21 @@ let configuration = "{" +
 	"\"name\":\"Alex\"," +
 	"\"accountType\":\"Premium\"," +
 	"\"email\":\"alex@swiftfuncional.com\"," +
-	"\"url\":\"www.swiftfuncional.com\","
+	"\"url\":\"www.swiftfuncional.com\"," +
 "}"
+
+struct Account {
+	let name: String
+	let accountType: String
+	let email: String
+	let url: String
+}
+
+func parse(json: String) -> Account {
+	let dict = try! JSONSerialization.jsonObject(with: json.data(using: .utf8)!) as! [String: Any]
+
+	return Account(name: dict["name"] as! String,
+	               accountType: dict["accountType"] as! String,
+	               email: dict["email"] as! String,
+	               url: dict["url"] as! String)
+}
