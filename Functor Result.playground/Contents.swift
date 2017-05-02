8527386717d04e8adb5a29ac9c908c1d27f2611c
Swift Funcional <info@swiftfuncional.com>
2017-05-02 21:40:04 +0200

    Copy functions of the previous playground

--- Functor Result.playground/Contents.swift
+++ Functor Result.playground/Contents.swift
@@ -11,3 +11,19 @@ enum Result<S, E> {
 		}
 	}
 }
+
+infix operator |>: AdditionPrecedence
+
+func |> <T, U, V>(first: @escaping (T) -> U, second: @escaping (U) -> V) -> (T) -> V {
+	return {
+		second(first($0))
+	}
+}
+
+func add2(to number: Int) -> Int {
+	return number + 2
+}
+
+func double(of number: Int) -> Int {
+	return number * 2
+}
