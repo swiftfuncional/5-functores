4c1661a6d690e085c0fd5e1308d30fe589b34254
Swift Funcional <info@swiftfuncional.com>
2017-05-02 21:37:48 +0200

    Create map function in Result functor

--- Functor Result.playground/Contents.swift
+++ Functor Result.playground/Contents.swift
@@ -1,4 +1,13 @@
 enum Result<S, E> {
 	case Success(_: S)
 	case Failure(_: E)
+
+	func map<T>(_ transform: (S) -> T) -> Result<T, E> {
+		switch self {
+		case let .Failure(reason):
+			return .Failure(reason)
+		case let .Success(value):
+			return .Success(transform(value))
+		}
+	}
 }
